81582abaefd526790e4a6e75c9fa24495babaeab
Felipe Gutierrez <Feguti4@gmail.com>
2022-05-31 14:42:30 -0300

    Sum operation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var operation = args[0];
+var n1 = Convert.ToDouble(args[1]);
+var n2 = Convert.ToDouble(args[2]);
+
+if (operation == "Sum")
+{
+    Console.WriteLine("\nResult:\n");
+    Console.WriteLine(n1 + n2);
+}
